9a6a4faca8d11b4abfff714588f91bd7f730aab6
CyrilAdolf <cyril.adolf@gmail.com>
2020-11-14 17:44:31 +0100

    add pagination function

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -7,23 +7,36 @@ import hero from "../Assets/img/hero.jpg";
 
 const Home = () => {
   const [offers, setOffers] = useState([]);
+  const [totalPage, setTotalPage] = useState();
+  const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
+
+  // Si je mets le numero de page dans un state? La requete va se relancer à chaque changement de page et limiter le temps de chargement.
+  // Il Faut définir le state à 1 par défaut. si on change de page il ne reviendra pas a 1 tout seul ?
+
   // AXIOS REQ
   useEffect(() => {
     const fetchdata = async () => {
       try {
         const response = await axios.get(
           // REPLACE WITH MY BACKEND URL
-          "https://lereacteur-vinted-api.herokuapp.com/offers"
+          `https://lereacteur-vinted-api.herokuapp.com/offers?limit=${8}&page=${page}`
         );
         setOffers(response.data.offers);
         setIsLoading(false);
+        setTotalPage(Math.ceil(response.data.count / 8));
       } catch (error) {
         console.log(error.message);
       }
     };
     fetchdata();
-  }, []);
+  }, [page]);
+
+  // DEFINE AN ARRAY WITH NUMBER FROM 1 TO TOTALPAGE
+  let arrayPage = [];
+  for (let i = 1; i <= totalPage; i++) {
+    arrayPage.push(i);
+  }
 
   return isLoading ? (
     // PACKAGE ARE AVAILABLE TO STYLE LOADING SCREEN
@@ -42,6 +55,24 @@ const Home = () => {
             </Link>
           );
         })}
+      </div>
+      <div className="pageInfo">
+        <p>Pages</p>
+        <div className="pageNumber">
+          {/* MAP OVER A LIST OF NUMBER */}
+          {arrayPage.map((page, i) => {
+            return (
+              <p
+                className="eachNumber"
+                onClick={() => {
+                  setPage(page);
+                }}
+              >
+                {page}
+              </p>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
